8d388be91c33cd152ab644605c35dfb13f07b7a6
mhkeller <code@mhkeller.com>
2020-04-12 19:58:43 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Block from './components/Block.svelte';
+	import Select from 'svelte-select';
+
+		let items = [
+			{value: 'chocolate', label: 'Chocolate'},
+			{value: 'pizza', label: 'Pizza'},
+			{value: 'cake', label: 'Cake'},
+			{value: 'chips', label: 'Chips'},
+			{value: 'ice-cream', label: 'Ice Cream'},
+		];
+
+		let selectedValue = undefined;
+	export let name;
+
+	const blocks = [0, 1, 2, 3, 4, 5];
+</script>
+
+<Select {items} bind:selectedValue></Select>
+<main>
+	{#each blocks as block, i}
+		<div class="block">
+			<h1>block {i}</h1>
+			<Block/>
+		</div>
+	{/each}
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+	main {
+		text-align: center;
+		width: 100%;
+		margin: 0 auto;
+	}
+
+	.block {
+		width: 100%;
+		height: 500px;
+		margin-bottom: 50px;
+		background-color: #f0c;
+	}
+
+	h1 {
+		position: absolute;
+	}
+
+</style>
---
+++ src/components/Block.svelte
@@ -0,0 +1,31 @@
+<script>
+
+  let clientWidth = 0;
+  let clientHeight = 0;
+</script>
+
+<div class="block-component"
+  bind:clientWidth
+  bind:clientHeight
+>
+<!-- <span>{clientWidth}x{clientHeight}</span> -->
+<svg
+  width={clientWidth}
+  height={clientHeight}
+>
+  <text x="0" y="20">{clientWidth}x{clientHeight}</text>
+</svg>
+</div>
+
+<style>
+  .block-component {
+    width: 100%;
+    height: 100%;
+    background-color: #fc0;
+  }
+  span, svg {
+    top: 0;
+    left: 0;
+    position: absolute;
+  }
+</style>
